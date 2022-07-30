8ce36d9c53c8f32c75adc418feeb700df88192ae
Phillip Rearick <psrearick@gmail.com>
2022-07-30 12:43:30 -0400

    wip

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Money with Phil',
+  siteUrl: 'https://www.philliprearick.com',
   plugins: []
 }

03bb9cf76a61aa6ac0ec4949c8ef3b32e5b921b4
Phillip Rearick <psrearick@gmail.com>
2022-08-02 14:10:11 -0400

    update fonts

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 require('./app.css');
 
+import "@fontsource/lato"
+import "@fontsource/ubuntu"
+
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {

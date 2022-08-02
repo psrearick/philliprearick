a97e0c3916f9f21618ee68979b909aa7b3a3f495
Phillip Rearick <psrearick@gmail.com>
2022-08-02 14:32:54 -0400

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended",
+        "plugin:prettier/recommended",
+    ],
+    "parserOptions": {
+        parser: "babel-eslint",
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        // we should always disable console logs and debugging in production
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "vue/no-unused-components": "off",
+        "vue/no-v-html": "off",
+    },
+};
